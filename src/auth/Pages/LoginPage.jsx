73d691c65aeb19dbb38f950368fcc77f3ff3c9fe
Jorge Mosqueda <mosqueda.ing@icloud.com>
2022-07-26 22:16:11 -0500

    Pagina login

--- src/auth/Pages/LoginPage.jsx
+++ src/auth/Pages/LoginPage.jsx
@@ -1,3 +1,60 @@
+import { Link as RouterLink } from "react-router-dom";
+import { Google } from "@mui/icons-material";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: "100vh", backgroundColor: "primary.main", padding: 4 }}
+    >
+      <Grid
+        item
+        className="box-shadow"
+        xs={3}
+        sx={{ backgroundColor: "white", padding: 3, borderRadius: 2 }}
+      >
+        <Typography variant="h5" sx={{ marginBottom: 1 }}>
+          Login
+        </Typography>
+        <form>
+          <Grid container>
+            <Grid item xs={12} sx={{ marginTop: 2 }}>
+              <TextField
+                label="email"
+                type="email"
+                placeholder="email@gmail.com"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sx={{ marginTop: 2 }}>
+              <TextField label="password" type="password" fullWidth />
+            </Grid>
+            <Grid container spacing={2} sx={{ marginBottom: 2 }}>
+              <Grid item xs={12} sm={6} sx={{ marginTop: 2 }}>
+                <Button variant="contained" fullWidth>
+                  Login
+                </Button>
+              </Grid>
+              <Grid item xs={12} sm={6} sx={{ marginTop: 2 }}>
+                <Button variant="contained" fullWidth>
+                  <Google />
+                  <Typography sx={{ marginLeft: 1 }}>Google</Typography>
+                </Button>
+              </Grid>
+            </Grid>
+            <Grid container direction="row" justifyContent="end">
+              <Link component={RouterLink} to="/auth/register">
+                Create account
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </Grid>
+    </Grid>
+  );
 };
