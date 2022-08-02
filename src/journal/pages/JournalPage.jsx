96e20fa70b58d55d985d3da4bb349953454a7e02
Jorge Mosqueda <mosqueda.ing@icloud.com>
2022-08-01 20:59:09 -0500

    Views del JournalLayout y componente ImageGalleru en el NoteView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,12 @@
-import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NoteView } from "../views/NoteView";
+import { NothingSelectedView } from "../views/NothingSelectedView";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography variant="h1">JournalPage</Typography>
+      {/* <NothingSelectedView /> */}
+      <NoteView />
     </JournalLayout>
   );
 };
---
+++ src/journal/views/NoteView.jsx
@@ -0,0 +1,47 @@
+import { SaveOutlined } from "@mui/icons-material";
+import { Button, Grid, TextField, Typography } from "@mui/material";
+import { ImageGallery } from "../components/ImageGallery";
+
+export const NoteView = () => {
+  return (
+    <Grid
+      container
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      sx={{ marginBottom: 2 }}
+    >
+      <Grid item>
+        <Typography fontSize={39} fontWeight="ligth">
+          28 Agosto 2023
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Button color="primary" sx={{ padding: 2 }}>
+          <SaveOutlined xs={{ fontsize: 30 }} />
+          Guardar
+        </Button>
+      </Grid>
+      <Grid container>
+        <TextField
+          type="text"
+          variant="filled"
+          fullWidth
+          placeholder="Ingrese un titulo"
+          label="Titulo"
+          sx={{ border: "none", marginBottom1: 1 }}
+        />
+        <TextField
+          type="text"
+          variant="filled"
+          multiline
+          fullWidth
+          placeholder="¿Qué sucedio el día de hoy?"
+          label="Titulo"
+          minRows={5}
+        />
+      </Grid>
+      <ImageGallery />
+    </Grid>
+  );
+};
